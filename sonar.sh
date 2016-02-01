72dcfb1b66ea88a016fee3e65e6aff4a4cdda557
Nicolas Sauvage <nicolas.sauvage@synaptix-labs.com>
2016-02-01 14:59:01 +0100

    update sonar script

--- sonar.sh
+++ sonar.sh
@@ -1,7 +1,3 @@
 if [ $TRAVIS_BRANCH == master ]; then
-    if [ $TRAVIS_PULL_REQUEST == false ]; then
         mvn --batch-mode verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar -Dsonar.host.url=${env.SONAR_URL} -Dsonar.login=${env.SONAR_LOGIN}
-    else
-        mvn --batch-mode verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar -Dsonar.host.url=${env.SONAR_URL} -Dsonar.login=${env.SONAR_LOGIN} -Dsonar.analysis.mode=preview -Dsonar.issuesReport.console.enable=true
-    fi
 fi
