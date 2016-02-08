1ce0037f10d34f364aae160a29ca2927295c5d31
Sallah Kokaina <sallah.kokaina@synaptix-labs.com>
2016-02-08 17:41:12 +0100

    adapaters config management

---
+++ toast-tk-adapters-api/src/main/java/com/synaptix/toast/adapter/constant/AdaptersConfig.java
@@ -0,0 +1,35 @@
+package com.synaptix.toast.adapter.constant;
+
+public class AdaptersConfig {
+
+	private String webDriver;
+
+	private String webDriverPath;
+
+	private boolean isSSl;
+
+	public String getWebDriver() {
+		return webDriver;
+	}
+
+	public void setWebDriver(String webDriver) {
+		this.webDriver = webDriver;
+	}
+
+	public String getWebDriverPath() {
+		return webDriverPath;
+	}
+
+	public void setWebDriverPath(String webDriverPath) {
+		this.webDriverPath = webDriverPath;
+	}
+
+	public boolean getIsSSl() {
+		return isSSl;
+	}
+
+	public void setIsSSl(boolean isSSl) {
+		this.isSSl = isSSl;
+	}
+
+}
