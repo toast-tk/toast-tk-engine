9198b9daca39a175a6ad2ae9924a5b4443afb265
Nicolas Sauvage <noki.sauvage@gmail.com>
2016-04-05 16:33:02 +0200

    repair merge request build
    change sonar analysis mode

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${TRAVIS_BRANCH} = 'master' ]; then
       mvn --batch-mode clean deploy org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} --settings ./settings.xml -PDEPLOY_NEXUS_SYNAPTIX;
 elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
       echo 'Only build and analyze pull request'
-      mvn --batch-mode clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar -Dsonar.analysis.mode=preview -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} -Dsonar.github.oauth=${SONAR_GITHUB_OAUTH} -Dsonar.github.repository=talan-labs/toast-tk-engine -Dsonar.github.pullRequest=${TRAVIS_PULL_REQUEST};
+      mvn --batch-mode clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar -Dsonar.analysis.mode=issues -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} -Dsonar.github.oauth=${SONAR_GITHUB_OAUTH} -Dsonar.github.repository=talan-labs/toast-tk-engine -Dsonar.github.pullRequest=${TRAVIS_PULL_REQUEST};
 fi
