98af36df44f66c3628252d0a7e3119a3c4386de6
Nicolas Sauvage <nicolas.sauvage@synaptix-labs.com>
2016-06-17 17:36:48 +0200

    WIP update file opening

--- toast-tk-runtime/src/main/java/com/synaptix/toast/runtime/AbstractScenarioRunner.java
+++ toast-tk-runtime/src/main/java/com/synaptix/toast/runtime/AbstractScenarioRunner.java
@@ -1,15 +1,25 @@
 package com.synaptix.toast.runtime;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -27,98 +37,102 @@ import com.synaptix.toast.runtime.utils.RunUtils;
 
 public abstract class AbstractScenarioRunner extends AbstractRunner {
 
-    private static final Logger LOG = LogManager.getLogger(AbstractScenarioRunner.class);
+	private static final Logger LOG = LogManager.getLogger(AbstractScenarioRunner.class);
 
-    private boolean presetRepoFromWebApp = false;
+	private boolean presetRepoFromWebApp = false;
 
-    private ITestPage localRepositoryTestPage;
+	private ITestPage localRepositoryTestPage;
 
-    private IHTMLReportGenerator htmlReportGenerator;
+	private IHTMLReportGenerator htmlReportGenerator;
 
-    private DefaultTestProgressReporter progressReporter;
+	private DefaultTestProgressReporter progressReporter;
 
-    protected AbstractScenarioRunner(final Injector injector) {
-    	super(injector);
-        this.htmlReportGenerator = injector.getInstance(IHTMLReportGenerator.class);
-        final EventBus eventBus = injector.getInstance(Key.get(EventBus.class, EngineEventBus.class));
-        this.progressReporter = new DefaultTestProgressReporter(eventBus);
-    }
-    
-    protected AbstractScenarioRunner(final Module m) {
-    	super(m);
-        this.htmlReportGenerator = injector.getInstance(IHTMLReportGenerator.class);
-        final EventBus eventBus = injector.getInstance(Key.get(EventBus.class, EngineEventBus.class));
-        this.progressReporter = new DefaultTestProgressReporter(eventBus);
-    }
+	protected AbstractScenarioRunner(final Injector injector) {
+		super(injector);
+		this.htmlReportGenerator = injector.getInstance(IHTMLReportGenerator.class);
+		final EventBus eventBus = injector.getInstance(Key.get(EventBus.class, EngineEventBus.class));
+		this.progressReporter = new DefaultTestProgressReporter(eventBus);
+	}
 
-    protected AbstractScenarioRunner() {
-    	super();
-        this.htmlReportGenerator = injector.getInstance(IHTMLReportGenerator.class);
-        final EventBus eventBus = injector.getInstance(Key.get(EventBus.class, EngineEventBus.class));
-        this.progressReporter = new DefaultTestProgressReporter(eventBus);
-    }
+	protected AbstractScenarioRunner(final Module m) {
+		super(m);
+		this.htmlReportGenerator = injector.getInstance(IHTMLReportGenerator.class);
+		final EventBus eventBus = injector.getInstance(Key.get(EventBus.class, EngineEventBus.class));
+		this.progressReporter = new DefaultTestProgressReporter(eventBus);
+	}
 
-    public final void run(
-    	final String... scenarios
-    ) throws Exception {
-        runScenario(scenarios);
-    }
+	protected AbstractScenarioRunner() {
+		super();
+		this.htmlReportGenerator = injector.getInstance(IHTMLReportGenerator.class);
+		final EventBus eventBus = injector.getInstance(Key.get(EventBus.class, EngineEventBus.class));
+		this.progressReporter = new DefaultTestProgressReporter(eventBus);
+	}
 
-    public final void runScenario(
-    	final String... scenarios
-    ) throws Exception {
-    	final List<ITestPage> testPages = new ArrayList<>();
-        initEnvironment();
-        for(final String fileName : scenarios) {
-            LOG.info("Start main test parser: {}", fileName);
-            final File file = readTestFile(fileName);
-            final ITestPage result = runScript(file, fileName);
-            testPages.add(result);
-        }
-        tearDownEnvironment();
-        LOG.info("{}file(s) processed", scenarios.length);
-        RunUtils.printResult(testPages);
-    }
+	public final void run(
+			final String... scenarios
+	) throws Exception {
+		runScenario(scenarios);
+	}
 
-    public final void runRemote(
-    	final String... scenarios
-    ) throws Exception {
-        this.presetRepoFromWebApp = true;
-        run(scenarios);
-    }
-
-    public final void runRemoteScript(
-    	final String script
-    ) throws Exception {
-        this.presetRepoFromWebApp = true;
-        runScript(null, script);
-    }
-
-    public void runLocalScript(
-    	final String wikiScenario,
-    	final String repoWiki,
-    	final IReportUpdateCallBack callback
-    ) throws IOException {
-        this.progressReporter.setReportCallBack(callback);
-        final TestParser parser = new TestParser();
-        this.localRepositoryTestPage = parser.readString(repoWiki, null);
-        runScript(null, wikiScenario);
-    }
-
-    private File readTestFile(
-    		final String fileName
-    ) throws IOException, URISyntaxException {
-    	File f = new File(fileName);
-		if(f.exists()){
-			return f;
+	private void runScenario(
+			final String... scenarios
+	) throws Exception {
+		final List<ITestPage> testPages = new ArrayList<>();
+		initEnvironment();
+		for (final String fileName : scenarios) {
+			LOG.info("Start main test parser: {}", fileName);
+			final File file = readTestFile(fileName);
+			final ITestPage result = runScript(file, fileName);
+			testPages.add(result);
 		}
+		tearDownEnvironment();
+		LOG.info("{}file(s) processed", scenarios.length);
+		RunUtils.printResult(testPages);
+	}
 
-    	final URL resource = this.getClass().getClassLoader().getResource(fileName);
-        if (resource == null) {
-            throw new FileNotFoundException(fileName);
-        }
-        return new File(resource.getFile());
-    }
+	public final void runRemote(
+			final String... scenarios
+	) throws Exception {
+		this.presetRepoFromWebApp = true;
+		run(scenarios);
+	}
+
+	public final void runRemoteScript(
+			final String script
+	) throws Exception {
+		this.presetRepoFromWebApp = true;
+		runScript(null, script);
+	}
+
+	public void runLocalScript(
+			final String wikiScenario,
+			final String repoWiki,
+			final IReportUpdateCallBack callback
+	) throws IOException {
+		this.progressReporter.setReportCallBack(callback);
+		final TestParser parser = new TestParser();
+		this.localRepositoryTestPage = parser.readString(repoWiki, null);
+		runScript(null, wikiScenario);
+	}
+
+	private File readTestFile(
+			final String fileName
+	) throws IOException, URISyntaxException {
+		InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(fileName);
+
+		final File tempFile = File.createTempFile("test", "tmp");
+		tempFile.deleteOnExit();
+		try (FileOutputStream out = new FileOutputStream(tempFile)) {
+			IOUtils.copy(resourceAsStream, out);
+		}
+		return tempFile;
+
+//		final URI uri = resource.toURI();
+//		final Map<String, String> env = new HashMap<>();
+//		final String[] array = uri.toString().split("!");
+//		final FileSystem fs = FileSystems.newFileSystem(URI.create(array[0]), env);
+//		final Path path = fs.getPath(array[1]);
+	}
 
     private ITestPage runScript(
     	final File file,
@@ -143,13 +157,13 @@ public abstract class AbstractScenarioRunner extends AbstractRunner {
         return result;
     }
 
-    protected void createAndOpenReport(
-    	final ITestPage testPage
-    ) {
-    	final String generatePageHtml = htmlReportGenerator.generatePageHtml(testPage);
-    	final String path = getReportsFolderPath();
-        final String pageName = testPage.getName();
-        htmlReportGenerator.writeFile(generatePageHtml, pageName, path);
-        openReport(path, pageName);
-    }
+	private void createAndOpenReport(
+			final ITestPage testPage
+	) {
+		final String generatePageHtml = htmlReportGenerator.generatePageHtml(testPage);
+		final String path = getReportsFolderPath();
+		final String pageName = testPage.getName();
+		htmlReportGenerator.writeFile(generatePageHtml, pageName, path);
+		openReport(path, pageName);
+	}
 }
