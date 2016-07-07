9ccc008a91389c28ad55754d1a9110e8762805ef
Sallah Kokaina <sallah.kokaina@gmail.com>
2016-07-07 10:27:26 +0200

    publish snapshot only

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+if [ $TRAVIS_PULL_REQUEST == "false" ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
     mvn deploy --settings ./settings.xml -Possrh,deploy
     exit $?
 fi
