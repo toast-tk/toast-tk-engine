9e6cb1b0fc0dcd2a7eee7b5af646b9e0f9bc847a
Sallah Kokaina <sallah.kokaina@gmail.com>
2016-07-10 19:27:56 +0200

    effective settings removal

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ $TRAVIS_PULL_REQUEST == "false" ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
-	mvn help:effective-settings --settings ./settings.xml  -DshowPasswords=true
     mvn deploy --settings ./settings.xml -DskipTests=true -Pdeploy;
     exit $?;
 fi
