d3c203d0ac6936c2b40af3b0345133108fa9ab49
Sallah Kokaina <sallah.kokaina@gmail.com>
2016-08-02 14:15:37 +0100

    adding missing build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${TRAVIS_BRANCH} = 'master' ]; then
+      echo 'Build master branch'
+      mvn --batch-mode clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} --settings ./settings.xml;
+elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
+      echo 'Only build and analyze pull request'
+      mvn --batch-mode clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar -Dsonar.analysis.mode=issues -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} -Dsonar.github.oauth=${SONAR_GITHUB_OAUTH} -Dsonar.github.repository=talan-labs/toast-tk-engine -Dsonar.github.pullRequest=${TRAVIS_PULL_REQUEST};
+fi
