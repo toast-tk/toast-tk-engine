c4676781680ad3f38e7bd2ad031fa81e0f8f83dc
Sallah Kokaina <sallah.kokaina@gmail.com>
2016-08-02 14:27:56 +0100

    fixing publish script

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if [ $TRAVIS_PULL_REQUEST == "false" ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; 
-then
+if [ $TRAVIS_PULL_REQUEST == "false" ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
     mvn clean deploy --settings ./settings.xml -DskipTests=true -Pdeploy -B -U;
     exit $?;
-else if [ ! -z "$TRAVIS_TAG" ] && [ ${TRAVIS_BRANCH} = 'master' ]
-then
- 	mvn --settings ./settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+elif [ ! -z "$TRAVIS_TAG" ] && [ ${TRAVIS_BRANCH} = 'master' ]; then
+ 	mvn --settings ./settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null;
 	mvn clean deploy --settings ./settings.xml -DskipTests=true -Pdeploy -B -U;
 	exit $?;
 fi
