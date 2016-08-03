b3f6bba0ccb2b033e78fe8bb17b2ebcaf125d395
Sallah Kokaina <sallah.kokaina@gmail.com>
2016-08-03 23:57:06 +0100

    tag deploy condition

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -7,7 +7,7 @@ if [ $TRAVIS_PULL_REQUEST == "false" ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; the
     echo 'Deploying snapshot to OSS'
     mvn clean deploy --settings ./settings.xml -DskipTests=true -Pdeploy -B -U;
     exit $?;
-elif [ ! -z "$TRAVIS_TAG" ] && [ ${TRAVIS_BRANCH} = 'master' ]; then
+elif [ ! -z "$TRAVIS_TAG" ]; then
 	echo 'Deploying release tag to OSS'
  	mvn --settings ./settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG;
 	mvn clean deploy --settings ./settings.xml -DskipTests=true -Pdeploy -B -U;
