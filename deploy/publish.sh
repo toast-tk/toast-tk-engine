5024d8367bf2b6ec3e1b707db60001413ab5f924
Sallah Kokaina <sallah.kokaina@gmail.com>
2016-08-04 00:19:36 +0100

    fix release deploy

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -5,11 +5,11 @@ echo "Preparing deploy for tag: $TRAVIS_TAG"
 
 if [ $TRAVIS_PULL_REQUEST == "false" ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
     echo 'Deploying snapshot to OSS'
-    mvn clean deploy --settings ./settings.xml -DskipTests=true -Pdeploy -B -U;
+    mvn clean deploy --settings ./settings.xml -DskipTests=true -Pdeploy;
     exit $?;
 elif [ ! -z "$TRAVIS_TAG" ]; then
 	echo 'Deploying release tag to OSS'
  	mvn --settings ./settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG;
-	mvn clean deploy --settings ./settings.xml -DskipTests=true -Pdeploy -B -U;
+	mvn clean deploy --settings ./settings.xml -DskipTests=true -Pdeploy;
 	exit $?;
 fi
