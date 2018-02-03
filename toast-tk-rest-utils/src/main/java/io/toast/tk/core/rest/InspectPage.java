b474aa8865ff4ecf55b0694a4d8213c6bf91005e
Sallah Kokaina <cbox@MacBook-Pro-de-KOKAINA.local>
2018-02-03 12:16:49 +0000

    (feat) code pourrie

--- toast-tk-rest-utils/src/main/java/io/toast/tk/core/rest/InspectPage.java
+++ toast-tk-rest-utils/src/main/java/io/toast/tk/core/rest/InspectPage.java
@@ -9,18 +9,32 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class InspectPage {
 
 	public String name;
+	
+	public Double erreur;
 
 	public List<String> items = new ArrayList<>();
 
 	protected InspectPage() {
-
+		this.erreur = null;
 	}
 
 	public InspectPage(
 		final String item,
 		final List<Object> items
 	) {
+		this.erreur = null;
 		this.name = item;
 		items.stream().forEach(o -> items.add(o.toString()));
+	}
+	
+	private void codepouri() {
+		if(erreur.equals("")) {
+			throw new IllegalAccessError("Meetup");
+		}
+		if(erreur == 0.0) {
+			throw new IllegalAccessError();
+		}
+		System.out.println("");
+		return;
 	}
 }
